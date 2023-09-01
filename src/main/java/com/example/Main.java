4c6395263a36fa050396e93ee2b91e27da060c41
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-09-01 18:29:12 +0000

    Streams intro

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,13 +1,16 @@
 package com.example;
 
 import java.util.ArrayList;
+//import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    Integer[] scores = new Integer[]{80, 66, 73, 92, 43};
+    //Integer[] scores = new Integer[]{80, 66, 73, 92, 43};
+    //Stream<Integer> scoreStream = Arrays.stream(scores);
 
     List<String> shoppingList = new ArrayList<>();
     shoppingList.add("coffee");
@@ -15,6 +18,16 @@ public class Main {
     shoppingList.add("pineapple");
     shoppingList.add("milk");
     shoppingList.add("pasta");
+
+    Stream<String> shoppingListStream = shoppingList.stream();
+
+    //Stream<String> letterStream = Stream.of("a", "b", "c");
+
+    shoppingListStream
+      .sorted()
+      .map((item) -> item.toUpperCase())
+      .filter((item) -> item.startsWith("P"))
+      .forEach(System.out::println);;
 
   }
 }
