0d54624dcf89363f725f630003b9f19e60ae52f1
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-09-01 18:35:46 +0000

    Streams Pt. 2

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,13 +3,14 @@ package com.example;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    Stream<String> lettersStream = Stream.of("a", "b", "c");
+    //Stream<String> lettersStream = Stream.of("a", "b", "c");
 
     Integer[] scores = new Integer[]{80, 66, 73, 92, 43};
     Stream<Integer> scoresStream = Arrays.stream(scores);
@@ -20,9 +21,17 @@ public class Main {
     shoppingList.add("pineapple");
     shoppingList.add("milk");
     shoppingList.add("pasta");
+    
     Stream<String> shoppingListStream = shoppingList.stream();
-    shoppingListStream.sorted()
-            .forEach(item -> System.out.println(item));
+    
+    List<String> sorted_shoppingList = shoppingListStream
+      .sorted()
+      .map((item) -> item.toUpperCase())
+      .filter((item) -> item.startsWith("P"))
+      .collect(Collectors.toList());
+
+    System.out.println(sorted_shoppingList);
+    System.out.println(shoppingList);
 
   }
 }
